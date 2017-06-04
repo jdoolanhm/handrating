67d7970594665675e35e9440cb39c6c047ca08e4
312dools <312dools@gmail.com>
2017-06-03 20:09:05 -0400

    RICARDO

---
+++ Handrating/OCR Tutorial Resources 3/LoveInASnap/FakeObjectiveC-Class.h
@@ -0,0 +1,13 @@
+//
+//  FakeObjectiveC-Class.h
+//  LoveInASnap
+//
+//  Created by Richard Hausman on 6/3/17.
+//  Copyright © 2017 Lyndsey Scott. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FakeObjectiveC_Class : NSObject
+
+@end
---
+++ Handrating/OCR Tutorial Resources 3/LoveInASnap/FakeObjectiveC-Class.m
@@ -0,0 +1,13 @@
+//
+//  FakeObjectiveC-Class.m
+//  LoveInASnap
+//
+//  Created by Richard Hausman on 6/3/17.
+//  Copyright © 2017 Lyndsey Scott. All rights reserved.
+//
+
+#import "FakeObjectiveC-Class.h"
+
+@implementation FakeObjectiveC_Class
+
+@end
